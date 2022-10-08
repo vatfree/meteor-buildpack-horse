ed1dee3f661218fa102868fd91cd3bf26aa3a1b9
productiveme <jaco@productive.me>
2022-10-08 17:24:49 +0200

    Do cleanup in the buildpack

--- extra/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $COMPILE_DIR
-
-echo 'Cleaning up $(pwd) ..........'
-find . -type f -name 'README.md' -delete \
-    && find . -type f -name 'LICENSE' -delete \
-    && find . -type f -name '*.d.ts' -delete \
-    && rm -r ./npm/node_modules/meteor/babel-compiler/node_modules/typescript

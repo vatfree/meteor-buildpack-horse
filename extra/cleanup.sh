24e0e60b6e41326f146e2a1387a8da5761d83a17
productiveme <jaco@productive.me>
2022-10-08 12:58:55 +0200

    Add output when cleaning up

--- extra/cleanup.sh
+++ extra/cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd $COMPILE_DIR
+
+echo 'Cleaning up $(pwd) ..........'
 find . -type f -name 'README.md' -delete \
     && find . -type f -name 'LICENSE' -delete \
     && find . -type f -name '*.d.ts' -delete \
